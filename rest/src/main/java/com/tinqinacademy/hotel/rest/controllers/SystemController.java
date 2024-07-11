d0edaf52aacca54aabf9deaf263db573b6e23c9f
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-07-11 10:25:23 +0300

    Delete Room operation

--- rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/SystemController.java
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/SystemController.java
@@ -16,9 +16,9 @@ import com.tinqinacademy.hotel.core.services.SystemService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/system")
