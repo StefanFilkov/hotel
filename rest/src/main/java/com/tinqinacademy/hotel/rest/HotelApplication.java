4e0b6c86bfaa5f304449586807584a7cc2ca4a0b
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-07-11 10:24:04 +0300

    test

---
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/HotelApplication.java
@@ -0,0 +1,16 @@
+package com.tinqinacademy.hotel.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.tinqinacademy.hotel")
+public class HotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApplication.class, args);
+    }
+
+}
