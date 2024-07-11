d982962788b379add1dad0f789a14ea302d8cee4
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-07-11 10:29:02 +0300

    Delete Booking operation

---
+++ api/src/main/java/com.tinqinacademy.hotel.api/operations/deletebookingbyid/DeleteBookingByIdOutput.java
@@ -0,0 +1,11 @@
+package com.tinqinacademy.hotel.api.operations.deletebookingbyid;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class DeleteBookingByIdOutput {
+    private final String message = "Delete successful";
+}
