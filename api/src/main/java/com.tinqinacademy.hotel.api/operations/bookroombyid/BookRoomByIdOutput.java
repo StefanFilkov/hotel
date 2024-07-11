ca98f54069ad6e45348f49a6c38b67542a401a0f
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-07-11 10:27:46 +0300

    Book Room operation

---
+++ api/src/main/java/com.tinqinacademy.hotel.api/operations/bookroombyid/BookRoomByIdOutput.java
@@ -0,0 +1,11 @@
+package com.tinqinacademy.hotel.api.operations.bookroombyid;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class BookRoomByIdOutput {
+    private final String message = "Booking successful";
+}
