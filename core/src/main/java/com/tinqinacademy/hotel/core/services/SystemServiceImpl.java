6662e02b52a7c8009c7918277c9d424e416d93d4
Stefan Filkov <118557566+StefanFilkov@users.noreply.github.com>
2024-07-29 18:05:45 +0300

    fixed url mappings

--- core/src/main/java/com/tinqinacademy/hotel/core/services/SystemServiceImpl.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/services/SystemServiceImpl.java
@@ -210,7 +210,10 @@ public class SystemServiceImpl implements SystemService {
     @Override
     public DeleteRoomOutput deleteRoom(DeleteRoomInput input) {
         log.info("Start deleteRoom input: {}", input.toString());
+
+        roomRepository.deleteById(UUID.fromString(input.getId()));
         DeleteRoomOutput result = DeleteRoomOutput.builder().build();
+
         log.info("End of deleteRoom result: {}", result.toString());
         return result;
     }
